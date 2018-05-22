34b7ff11fb8bf37674cdd8303f91190bad239783
andyhu92 <cihanghu@gmail.com>
2018-05-22 18:22:25 -0400

    Update package.json, add code syntax highlight

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { render } from "react-dom";
 import { ValidationForm, TextInput } from "../../lib";
-import "./styles.css";
+import './index.css';
 
 class Demo extends React.Component{
   state = {
     isShow:true
   }
+
+  componentDidMount() {
+    this.initCodeSyntaxHighlight();
+  }
+
+  initCodeSyntaxHighlight(){
+    let codes = document.getElementsByTagName("code");
+    for(let i = 0; i < codes.length; i++){
+      hljs.highlightBlock(codes[i]);  
+    }
+  }
+
   handleSubmit = (e, formData) => {
     e.preventDefault();
     console.log(formData);
@@ -14,16 +26,45 @@ class Demo extends React.Component{
 
   render () {
     return (
-      <ValidationForm className="container" onSubmit={this.handleSubmit} >
-        <h1>Demo Form</h1>
-        <div className="form-group">
-          <TextInput name="firstName" required/>
-        </div>
+      <section className="my-3 container">
+        <div className="row">
+          <div className="col-md-5">
+            <ValidationForm onSubmit={this.handleSubmit} >
+              <h4>React Bootstrap4 Form Validation</h4>
+              <div className="form-group">
+                <label htmlFor="firstName">First name</label>
+                <TextInput name="firstName" id="firstName" required/>
+              </div>
 
-        <div className="form-group">
-          <button className="btn btn-primary">Submit</button>
+              <div className="form-group">
+                <button className="btn btn-primary">Submit</button>
+              </div>
+            </ValidationForm>
+          </div>
+          <div className="col-md-7">
+              <pre>
+                <code className="lang-javascript">
+{`import { ValidationForm, TextInput } from "react-bootstrap4-form-validation"
+
+<ValidationForm onSubmit={this.handleSubmit} >
+  <h4>React Bootstrap4 Form Validation</h4>
+  <div className="form-group">
+    <label for="firstName">First name</label>
+    <TextInput name="firstName" id="firstName" required/>
+  </div>
+
+  <div className="form-group">
+    <button className="btn btn-primary">Submit</button>
+  </div>
+</ValidationForm>
+`}
+                </code>
+              </pre>
+          </div>
         </div>
-      </ValidationForm>
+     
+      </section>
+    
     );
   }
 }
